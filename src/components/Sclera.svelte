38fc36576da9f760ecc53a7c1fc713113fb1a034
DiogoRDuarte <diogorduarte00@gmail.com>
2023-08-01 00:42:35 +0100

    First commit

---
+++ src/components/Sclera.svelte
@@ -0,0 +1,22 @@
+<script>
+  import eyeSclera from "../data/sclera";
+  import * as d3 from "d3";
+
+  export let premiered;
+
+  console.log();
+
+  let season = premiered.split(" ")[0];
+
+  const seasonsArray = Object.keys(eyeSclera[0]);
+  const pathsArray = Object.values(eyeSclera[0]);
+
+  var correspondingPath = d3
+    .scaleOrdinal()
+    .domain(seasonsArray)
+    .range(pathsArray);
+
+  $: svgContent = correspondingPath(season);
+</script>
+
+{@html svgContent}
